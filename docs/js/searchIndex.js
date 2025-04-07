2f47ae19f1b6001f8c47aa397088fbfba2c87399
Juan Marí Ibáñez <juanmari@paucasesnovescifp.cat>
2025-04-07 12:39:00 +0200

    añado carpeta doc

--- docs/js/searchIndex.js
+++ docs/js/searchIndex.js
@@ -3,57 +3,57 @@ Search.appendIndex(
                 {
             "fqsen": "\\Actualitzar",
             "name": "Actualitzar",
-            "summary": "",
+            "summary": "Clase\u0020encargada\u0020de\u0020actualizar\u0020un\u0020producto\u0020en\u0020la\u0020base\u0020de\u0020datos",
             "url": "classes/Actualitzar.html"
         },                {
             "fqsen": "\\Actualitzar\u003A\u003Aactualizar\u0028\u0029",
             "name": "actualizar",
-            "summary": "",
+            "summary": "Actualiza\u0020un\u0020producto\u0020en\u0020la\u0020base\u0020de\u0020datos.",
             "url": "classes/Actualitzar.html#method_actualizar"
         },                {
             "fqsen": "\\Eliminar",
             "name": "Eliminar",
-            "summary": "",
+            "summary": "Script\u0020para\u0020eliminar\u0020un\u0020producto\u0020de\u0020la\u0020base\u0020de\u0020datos",
             "url": "classes/Eliminar.html"
         },                {
             "fqsen": "\\Eliminar\u003A\u003AeliminarProducte\u0028\u0029",
             "name": "eliminarProducte",
-            "summary": "",
+            "summary": "Elimina\u0020un\u0020producto\u0020por\u0020su\u0020ID",
             "url": "classes/Eliminar.html#method_eliminarProducte"
         },                {
             "fqsen": "\\Connexio",
             "name": "Connexio",
-            "summary": "",
+            "summary": "Gestiona\u0020la\u0020conexi\u00F3n\u0020a\u0020la\u0020base\u0020de\u0020datos\u0020\u0060la_meva_botiga\u0060\u0020utilizando\u0020\u0060mysqli\u0060.",
             "url": "classes/Connexio.html"
         },                {
             "fqsen": "\\Connexio\u003A\u003AobtenirConnexio\u0028\u0029",
             "name": "obtenirConnexio",
-            "summary": "",
+            "summary": "Establece\u0020y\u0020devuelve\u0020una\u0020conexi\u00F3n\u0020activa\u0020con\u0020la\u0020base\u0020de\u0020datos",
             "url": "classes/Connexio.html#method_obtenirConnexio"
         },                {
             "fqsen": "\\Connexio\u003A\u003A\u0024host",
             "name": "host",
-            "summary": "",
+            "summary": "Nombre\u0020del\u0020host\u0020de\u0020la\u0020base\u0020de\u0020datos",
             "url": "classes/Connexio.html#property_host"
         },                {
             "fqsen": "\\Connexio\u003A\u003A\u0024usuario",
             "name": "usuario",
-            "summary": "",
+            "summary": "Nombre\u0020de\u0020usuario\u0020para\u0020acceder\u0020a\u0020la\u0020base\u0020de\u0020datos",
             "url": "classes/Connexio.html#property_usuario"
         },                {
             "fqsen": "\\Connexio\u003A\u003A\u0024contrase\u00F1a",
             "name": "contrase\u00F1a",
-            "summary": "",
+            "summary": "Contrase\u00F1a\u0020del\u0020usuario\u0020de\u0020la\u0020base\u0020de\u0020datos",
             "url": "classes/Connexio.html#property_contrase%C3%B1a"
         },                {
             "fqsen": "\\Connexio\u003A\u003A\u0024baseDatos",
             "name": "baseDatos",
-            "summary": "",
+            "summary": "Nombre\u0020de\u0020la\u0020base\u0020de\u0020datos\u0020a\u0020la\u0020que\u0020se\u0020conecta",
             "url": "classes/Connexio.html#property_baseDatos"
         },                {
             "fqsen": "\\Footer",
             "name": "Footer",
-            "summary": "",
+            "summary": "Clase\u0020Footer\u0020para\u0020mostrar\u0020el\u0020pie\u0020de\u0020p\u00E1gina\u0020de\u0020la\u0020aplicaci\u00F3n",
             "url": "classes/Footer.html"
         },                {
             "fqsen": "\\Footer\u003A\u003AmostrarFooter\u0028\u0029",
@@ -63,32 +63,32 @@ Search.appendIndex(
         },                {
             "fqsen": "\\Header",
             "name": "Header",
-            "summary": "",
+            "summary": "Clase\u0020Header\u0020para\u0020generar\u0020el\u0020encabezado\u0020de\u0020la\u0020aplicaci\u00F3n",
             "url": "classes/Header.html"
         },                {
             "fqsen": "\\Header\u003A\u003AmostrarHeader\u0028\u0029",
             "name": "mostrarHeader",
-            "summary": "",
+            "summary": "Muestra\u0020el\u0020encabezado\u0020HTML\u0020completo\u0020con\u0020barra\u0020de\u0020navegaci\u00F3n\u0020y\u0020carrusel",
             "url": "classes/Header.html#method_mostrarHeader"
         },                {
             "fqsen": "\\Modificar",
             "name": "Modificar",
-            "summary": "",
+            "summary": "Formulario\u0020para\u0020modificar\u0020un\u0020producto\u0020existente",
             "url": "classes/Modificar.html"
         },                {
             "fqsen": "\\Modificar\u003A\u003AmostrarFormulari\u0028\u0029",
             "name": "mostrarFormulari",
-            "summary": "",
+            "summary": "Muestra\u0020el\u0020formulario\u0020de\u0020modificaci\u00F3n\u0020con\u0020los\u0020datos\u0020actuales\u0020del\u0020producto",
             "url": "classes/Modificar.html#method_mostrarFormulari"
         },                {
             "fqsen": "\\Inserir",
             "name": "Inserir",
-            "summary": "",
+            "summary": "Procesamiento\u0020de\u0020altas\u0020de\u0020productos\u0020\u002D\u0020Formulario\u0020CRUD",
             "url": "classes/Inserir.html"
         },                {
             "fqsen": "\\Inserir\u003A\u003AafegirProducte\u0028\u0029",
             "name": "afegirProducte",
-            "summary": "",
+            "summary": "Inserta\u0020un\u0020nuevo\u0020producto\u0020en\u0020la\u0020base\u0020de\u0020datos",
             "url": "classes/Inserir.html#method_afegirProducte"
         },                {
             "fqsen": "\\Principal",
@@ -98,7 +98,7 @@ Search.appendIndex(
         },                {
             "fqsen": "\\Principal\u003A\u003AmostrarProductes\u0028\u0029",
             "name": "mostrarProductes",
-            "summary": "",
+            "summary": "Muestra\u0020la\u0020lista\u0020de\u0020productos\u0020en\u0020formato\u0020de\u0020tabla\u0020HTML",
             "url": "classes/Principal.html#method_mostrarProductes"
         },                {
             "fqsen": "\\",
